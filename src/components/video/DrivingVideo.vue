07b189075a05760d7f038b0a164c30a564523cc0
h_admin <709820314@qq.com>
2021-04-13 16:38:06 +0800

    '接入视频管理'

--- src/components/video/DrivingVideo.vue
+++ src/components/video/DrivingVideo.vue
@@ -1,15 +1,7 @@
 <template>
     <div id="driving-video">
-        <!--    面包屑导航区-->
-        <!--        <el-breadcrumb separator-class="el-icon-arrow-right">-->
-        <!--            <el-breadcrumb-item :to="{path:'/adminHome'}">首页</el-breadcrumb-item>-->
-        <!--            <el-breadcrumb-item>视频管理</el-breadcrumb-item>-->
-        <!--        </el-breadcrumb>-->
         <el-card v-loading="loading">
-            <el-form :inline="true" :model="videoForm" ref="searchUserFormRef">
-                <el-form-item label="视频编号" prop="video_id">
-                    <el-input placeholder="请输入账号ID" v-model="videoForm.video_id"></el-input>
-                </el-form-item>
+            <el-form :inline="true" ref="searchUserFormRef">
                 <el-form-item label="拍摄时间" prop="selectedDate">
                     <el-date-picker
                             end-placeholder="结束日期"
@@ -18,43 +10,49 @@
                             range-separator="至"
                             start-placeholder="开始日期"
                             type="daterange"
-                            v-model="videoForm.selectedDate"
+                            v-model="selectedDate"
                             value-format="yyyy-MM-dd">
                     </el-date-picker>
                 </el-form-item>
                 <el-form-item>
-                    <el-button icon="el-icon-search" plain size="small" type="primary">查询</el-button>
+                    <el-button icon="el-icon-search" plain size="small" type="primary" @click="searchVideo">查询</el-button>
                 </el-form-item>
             </el-form>
             <el-table :data="this.videoList" border stripe>
                 <el-table-column align="center" label="序号" type="index" width="100px"></el-table-column>
-                <el-table-column align="center" label="视频编号" prop="video_id"></el-table-column>
-                <el-table-column align="center" label="登录账号" prop="user_id"></el-table-column>
-                <el-table-column align="center" label="拍摄时间" prop="video_time"></el-table-column>
-                <el-table-column align="center" label="记录时长" prop="video_duration"></el-table-column>
-                <el-table-column align="center" label="操作" width="300px">
+                <el-table-column align="center" label="视频编号" prop="videoID"></el-table-column>
+                <el-table-column align="center" label="拍摄时间" prop="videoTime"></el-table-column>
+                <el-table-column align="center" label="记录时长" prop="videoDuration"></el-table-column>
+                <el-table-column align="center" label="操作">
                     <template slot-scope="scope">
-                        <el-button @click="scanVideo(scope.row.id)" icon="el-icon-view" size="mini" type="primary">预览</el-button>
+                        <el-button @click="scanVideo(scope.row.videoLink)" icon="el-icon-view" size="mini" type="primary">预览</el-button>
                         <el-button @click="downloadVideo(scope.row.id)" icon="el-icon-download" size="mini" type="warning">下载</el-button>
+                        <el-button @click="deleteVideo(scope.row.id)" icon="el-icon-delete" size="mini" type="danger">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
         </el-card>
+        <el-dialog title="视频预览" :visible.sync="scanDialogVisible" width="50%" @close="scanDialogVisibleClosed">
+            <vue-core-video-player class="video-player" :src="videoSrc"></vue-core-video-player>
+        </el-dialog>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import VueCoreVideoPlayer from 'vue-core-video-player'
+
+    Vue.use(VueCoreVideoPlayer)
+
     export default {
         name: "DrivingVideo",
         data() {
             return {
-                videoForm: {
-                    video_id: '',
-                    user_name: '',
-                    selectedDate: ''
-                },
                 loading: true,
-                videoList: []
+                videoList: [],
+                selectedDate: [],
+                scanDialogVisible: false,
+                videoSrc: null
             }
         },
         created() {
@@ -62,8 +60,32 @@
         },
         methods: {
             async getVideoList() {
-
+                const {data:res} = await this.$http.post('driveVideo',{'user_name':window.sessionStorage.getItem('userName')})
+                if (res.code !== 200) {
+                    return this.$message.error("获取视频失败")
+                }
+                this.videoList = res.data.videos
                 this.loading = false
+            },
+            async searchVideo() {
+                const searchData = {'user_name':window.sessionStorage.getItem('userName')}
+                if (this.selectedDate !== null && this.selectedDate.length > 0) {
+                    searchData['start'] = this.selectedDate[0]
+                    searchData['end'] = this.selectedDate[1]
+                }
+                console.log(searchData)
+                const {data: res} = await this.$http.post('driveVideo', searchData)
+                if (res.code !== 200) {
+                    return this.$message.error("查询失败")
+                }
+                this.videoList = res.data.videos
+            },
+            scanDialogVisibleClosed() {
+                this.videoSrc = null
+            },
+            scanVideo(videoLink) {
+                this.scanDialogVisible = true
+                this.videoSrc = videoLink
             }
         }
     }
